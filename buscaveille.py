099165b148cf7d757c435fa1738cd7ed234e7ec8
MOON <133333114+M00N69@users.noreply.github.com>
2024-08-14 11:18:14 +0200

    Update buscaveille.py

--- buscaveille.py
+++ buscaveille.py
@@ -1,17 +1,18 @@
 import streamlit as st
-
-st.title("Test de Déploiement Simple")
-
-st.write("Si vous voyez ce message, l'application fonctionne correctement.")
-
-# Essayez de charger et d'afficher une simple DataFrame
 import pandas as pd
+import requests
+from io import BytesIO
 
 @st.cache_data
 def load_data():
-    file_path = "https://www.plateforme-sca.fr/media/11/download"
+    file_url = "https://www.plateforme-sca.fr/media/11/download"
     try:
-        df = pd.read_excel(file_path, engine='openpyxl')
+        # Télécharger le fichier
+        response = requests.get(file_url)
+        response.raise_for_status()  # Vérifie que la requête a réussi
+
+        # Charger le fichier Excel depuis le contenu téléchargé
+        df = pd.read_excel(BytesIO(response.content), engine='openpyxl')
         return df
     except Exception as e:
         st.error(f"Erreur lors du chargement des données : {e}")
